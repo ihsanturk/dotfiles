d88a7220914ccfb6da5f53d31541acc7dd9b2378
ihsn <aliihsanturk@gmail.com>
2019-08-01 20:52:03 +0300

    Add: dwm gaps between windows, Edit: dwm border is 1px

--- dwm/Software/dwm/config.h
+++ dwm/Software/dwm/config.h
@@ -1,10 +1,11 @@
 /* See LICENSE file for copyright and license details. */ /* appearance */
-static const unsigned int borderpx  =  4;        /* border pixel of windows */
-static const unsigned int snap      = 16;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack:size=9" }; /* :antialias=true:hinting=true */
-static const char dmenufont[]       = "Hack:size=9";
+static const unsigned int borderpx  = 1;  /* border pixel of windows */
+static const unsigned int gappx     = 23;  /* gaps between windows */
+static const unsigned int snap      = 16; /* snap pixel */
+static const int showbar            = 0;  /* 0 means no bar */
+static const int topbar             = 1;  /* 0 means bottom bar */
+static const char *fonts[]          = { "Hack:size=10" }; /* :antialias=true:hinting=true */
+static const char dmenufont[]       = "Hack:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -53,7 +54,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static char dmenuline[2] = "5";
+static char dmenuline[2] = "10";
 static const char *dmenucmd[] = { "dmenu_run", "-l", dmenuline, "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
