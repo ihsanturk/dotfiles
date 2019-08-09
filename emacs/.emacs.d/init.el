39cf60b52b4a411abcab20dc125618fe0e4be66e
ihsn <aliihsanturk@gmail.com>
2019-08-09 18:13:24 +0300

    Edit: emacs configuration with org mode

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,26 @@
+;; Configure package.el to include MELPA.
+(require 'package)
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Ensure that use-package is installed.
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+	(smex flx counsel company org-bullets evil-surround evil which-key pdf-tools undo-tree telephone-line gruvbox-theme auto-compile use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
