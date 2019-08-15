6edeb067ea5a1756c90cc2bb3b4f09bdf79c8984
ihsn <ihsn@ihsns-MacBook-Pro.local>
2019-08-15 22:04:42 +0300

    Fix: emacs folder git mistake

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -10,17 +10,3 @@
   (package-install 'use-package))
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-	(markdown-mode smex flx counsel company org-bullets evil-surround evil which-key pdf-tools undo-tree telephone-line gruvbox-theme auto-compile use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
