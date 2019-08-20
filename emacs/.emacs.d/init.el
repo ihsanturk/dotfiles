8d7ff4a0f3adaf6dbdb4ccc66d926f85675ad560
ihsn <ihsn@ihsns-MacBook-Pro.local>
2019-08-20 14:59:06 +0300

    Add: emacs melpa-stable

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,6 +2,9 @@
 (require 'package)
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
 (package-initialize)
 
 ;; Ensure that use-package is installed.
