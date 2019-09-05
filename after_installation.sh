11015eeb521f979de89baac8fb34f0d709939a8a
ihsn <aliihsanurk@gmail.com>
2019-09-05 18:20:07 +0300

    Edit: tidied up, typo

--- after_installation.sh
+++ after_installation.sh
@@ -1,33 +1,39 @@
 #!/bin/sh
 
+#
+# Mainly focused to void linux
+#
+
+
 ## Packages
-# sudo pacman -S\
-# 	git\
-# 	zsh\
-# 	nnn\
-# 	fzf\
-# 	mpv\
-# 	stow\
-# 	xset\
-#   curl\
-# 	dmenu\
-# 	xclip\
-# 	hdparm\
-# 	neovim\
-# 	ntfs-3g\
-#   usbmuxd\
-# 	chromium\
-# 	setxkbmap\
-# 	youtube-dl\
-# 	alsa-utils\
-# 	#libao-devel\
-# 	#libX11-devel\
-# 	#libXft-devel\
-# 	#ncurses-devel\
-# 	#portmidi-devel\
-#   #libimobiledevice\
-# 	#libXinerama-devel\
-# 	the_silver_searcher
+#sudo xbps-install -S\
+#  git\
+#  zsh\
+#  nnn\
+#  fzf\
+#  mpv\
+#  stow\
+#  xset\
+#  curl\
+#  dmenu\
+#  xclip\
+#  hdparm\
+#  neovim\
+#  ntfs-3g\
+#  usbmuxd\
+#  chromium\
+#  setxbmap\
+#  youtube-dl\
+#  base-devel\
+#  alsa-utils\
+#  #libao-devel\
+#  #libX11-devel\
+#  #libXft-devel\
+#  #ncurses-devel\
+#  #portmidi-devel\
+#  libimobiledevice\
+#  libXinerama-devel\
+#  the_silver_searcher
 
 # cd /home/ihsn; git clone https://github.com/ihsanturk/dotfiles
 
@@ -37,12 +43,12 @@
 # cd /home/ihsn/sof
 # git clone https://github.com/ihsanturk/st && cd st && sudo make clean install
 
+PSK=""
 SSID=""
 USERNAME="ihsn"
-PSK=""
 
 ## Activate touchpad tapping
-# sudo sed -i '/Identifier.*touchpad/aOption "Tapping" "on"' /usr/share/X11/xorg.conf.d/40-libinput.conf
+#sudo sed -i '/Identifier.*touchpad/aOption "Tapping" "on"' /usr/share/X11/xorg.conf.d/40-libinput.conf
 
 ## Set up network
 # wpa_passphrase $SSID $PSK >> /etc/wpa_supplicant/wpa_supplicant.conf
@@ -51,21 +57,21 @@ PSK=""
 # sleep 5
 
 ## Update and install Xorg
-# sudo xbps-install -Syuv
-# sudo xbps-install xorg-minimal xf86-video-intel git curl base-devel
+#sudo xbps-install -Syuv
+# sudo xbps-install xorg-minimal xf86-video-intel curl base-devel
 
 ## Set up Font
-cd /home/$USERNAME && mkdir .fonts && cd .fonts &&
-curl https://raw.githubusercontent.com/IBM/plex/master/IBM-Plex-Mono/fonts/complete/ttf/IBMPlexMono-Text.ttf -o IBMPlexMono-Text.ttf && fc-cache
+#cd /home/$USERNAME && mkdir .fonts && cd .fonts &&
+#curl https://raw.githubusercontent.com/IBM/plex/master/IBM-Plex-Mono/fonts/complete/ttf/IBMPlexMono-Text.ttf -o IBMPlexMono-Text.ttf && fc-cache
 
 ## Set default shell zsh
-# sudo chsh -s $(which zsh) $USERNAME
-# sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+#sudo chsh -s $(which zsh) $USERNAME
+#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
 ## Grub kernel options
-# sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"/\1 iommu=soft nopti nouveau.modeset=0"/g'\
+#sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"/\1 iommu=soft nopti nouveau.modeset=0"/g'\
 #/etc/default/grub
-# grub-mkconfig -o /boot/grub/grub.cfg
-# reboot
+#grub-mkconfig -o /boot/grub/grub.cfg
+#reboot
