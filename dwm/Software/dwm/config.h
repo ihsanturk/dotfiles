0770870d648372062296eac19462ecb5dc60112d
ihsn <aliihsanturk@gmail.com>
2019-09-06 22:32:29 +0300

    Edit: dwm colors, border, tags, layout names

--- dwm/Software/dwm/config.h
+++ dwm/Software/dwm/config.h
@@ -1,16 +1,16 @@
 /* See LICENSE file for copyright and license details. */ /* appearance */
-static const unsigned int borderpx  = 2;  /* border pixel of windows */
-static const unsigned int gappx     = 25;  /* gaps between windows */
+static const unsigned int borderpx  = 5;  /* border pixel of windows */
+static const unsigned int gappx     = 35;  /* gaps between windows */
 static const unsigned int snap      = 16; /* snap pixel */
-static const int showbar            = 0;  /* 0 means no bar */
+static const int showbar            = 1;  /* 0 means no bar */
 static const int topbar             = 1;  /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack:size=10" }; /* :antialias=true:hinting=true */
-static const char dmenufont[]       = "Hack:size=10";
+static const char *fonts[]          = { "Hack:size=12" }; /* :antialias=true:hinting=true */
+static const char dmenufont[]       = "Hack:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray4[]       = "#222222";
+static const char col_cyan[]        = "#87B268";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -18,7 +18,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "I", "II", "III", "IV" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -30,15 +30,15 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.57; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.53; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "t",      tile },    /* first entry is default */
+	{ "f",      NULL },    /* no layout function means floating behavior */
+	{ "m",      monocle },
 };
 
 /* key definitions */
