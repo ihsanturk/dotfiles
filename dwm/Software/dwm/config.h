4a0c8e51238f56a3c790b82cdf23016733d31c93
ihsan <aliihsanturk@gmail.com>
2019-09-29 16:23:08 +0300

    Edit: font size

--- dwm/Software/dwm/config.h
+++ dwm/Software/dwm/config.h
@@ -10,8 +10,8 @@ static const unsigned int gappx     = 35;  /* gaps between windows */
 static const unsigned int snap      = 16; /* snap pixel */
 static const int showbar            = 1;  /* 0 means no bar */
 static const int topbar             = 1;  /* 0 means bottom bar */
-static const char *fonts[]          = { "FontAwesome:size=13", "monospace:size=13" }; /* :antialias=true:hinting=true */
-static const char dmenufont[]       = "FontAwesome:size=13";
+static const char *fonts[]          = { "FontAwesome:size=16", "monospace:size=13" }; /* :antialias=true:hinting=true */
+static const char dmenufont[]       = "FontAwesome:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
