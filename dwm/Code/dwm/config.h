cc88fbbfed04dc830901bcff6d40c38df61cd9db
ihsan <aliihsanturk@gmail.com>
2019-10-28 17:10:22 +0300

    Edit: not show bar at first

--- dwm/Code/dwm/config.h
+++ dwm/Code/dwm/config.h
@@ -8,7 +8,7 @@
 static const unsigned int borderpx  = 0;  /* border pixel of windows */
 static const unsigned int gappx     = 35;  /* gaps between windows */
 static const unsigned int snap      = 16; /* snap pixel */
-static const int showbar            = 1;  /* 0 means no bar */
+static const int showbar            = 0;  /* 0 means no bar */
 static const int topbar             = 1;  /* 0 means bottom bar */
 static const char *fonts[]          = { "Inconsolata:size=17", "monospace:size=13" }; /* :antialias=true:hinting=true */
 static const char dmenufont[]       = "Inconsolata:size=17";
