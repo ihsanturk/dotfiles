a1c3b92ad31f087ee3b8c787122d480ebb35e2b4
ihsan <aliihsanturk@gmail.com>
2019-10-28 17:09:53 +0300

    Edit: color vars, Add: tput color vars

--- doc/Document/ansi_colors.sh
+++ doc/Document/ansi_colors.sh
@@ -1,35 +1,72 @@
 #!/bin/sh
 
-txtblk='\033[0;30m' # Black - Regular
-txtred='\033[0;31m' # Red
-txtgrn='\033[0;32m' # Green
-txtylw='\033[0;33m' # Yellow
-txtblu='\033[0;34m' # Blue
-txtpur='\033[0;35m' # Purple
-txtcyn='\033[0;36m' # Cyan
-txtwht='\033[0;37m' # White
-bldblk='\033[1;30m' # Black - Bold
-bldred='\033[1;31m' # Red
-bldgrn='\033[1;32m' # Green
-bldylw='\033[1;33m' # Yellow
-bldblu='\033[1;34m' # Blue
-bldpur='\033[1;35m' # Purple
-bldcyn='\033[1;36m' # Cyan
-bldwht='\033[1;37m' # White
-unkblk='\033[4;30m' # Black - Underline
-undred='\033[4;31m' # Red
-undgrn='\033[4;32m' # Green
-undylw='\033[4;33m' # Yellow
-undblu='\033[4;34m' # Blue
-undpur='\033[4;35m' # Purple
-undcyn='\033[4;36m' # Cyan
-undwht='\033[4;37m' # White
-bakblk='\033[40m'   # Black - Background
-bakred='\033[41m'   # Red
-bakgrn='\033[42m'   # Green
-bakylw='\033[43m'   # Yellow
-bakblu='\033[44m'   # Blue
-bakpur='\033[45m'   # Purple
-bakcyn='\033[46m'   # Cyan
-bakwht='\033[47m'   # White
-txtrst='\033[0m'    # Text Reset
+fgblk='\033[0;30m' # Black - Regular
+fgred='\033[0;31m' # Red
+fggrn='\033[0;32m' # Green
+fgylw='\033[0;33m' # Yellow
+fgblu='\033[0;34m' # Blue
+fgpur='\033[0;35m' # Purple
+fgcyn='\033[0;36m' # Cyan
+fgwht='\033[0;37m' # White
+
+bdblk='\033[1;30m' # Black - Bold
+bdred='\033[1;31m' # Red
+bdgrn='\033[1;32m' # Green
+bdylw='\033[1;33m' # Yellow
+bdblu='\033[1;34m' # Blue
+bdpur='\033[1;35m' # Purple
+bdcyn='\033[1;36m' # Cyan
+bdwht='\033[1;37m' # White
+
+ulblk='\033[4;30m' # Black - underline
+ulred='\033[4;31m' # Red
+ulgrn='\033[4;32m' # Green
+ulylw='\033[4;33m' # Yellow
+ulblu='\033[4;34m' # Blue
+ulpur='\033[4;35m' # Purple
+ulcyn='\033[4;36m' # Cyan
+ulwht='\033[4;37m' # White
+
+bgblk='\033[40m'   # Black - Background
+bgred='\033[41m'   # Red
+bggrn='\033[42m'   # Green
+bgylw='\033[43m'   # Yellow
+bgblu='\033[44m'   # Blue
+bgpur='\033[45m'   # Purple
+bgcyn='\033[46m'   # Cyan
+bgwht='\033[47m'   # White
+fgrst='\033[0m'    # Text Reset
+
+
+# Tput
+
+bgBlack=$(tput setab 0) # black
+bgRed=$(tput setab 1) # red
+bgGreen=$(tput setab 2) # green
+bgYellow=$(tput setab 3) # yellow
+bgBlue=$(tput setab 4) # blue
+bgMagenta=$(tput setab 5) # magenta
+bgCyan=$(tput setab 6) # cyan
+bgWhite=$(tput setab 7) # white
+
+# foreground color using ANSI escape
+
+fgBlack=$(tput setaf 0) # black
+fgRed=$(tput setaf 1) # red
+fgGreen=$(tput setaf 2) # green
+fgYellow=$(tput setaf 3) # yellow
+fgBlue=$(tput setaf 4) # blue
+fgMagenta=$(tput setaf 5) # magenta
+fgCyan=$(tput setaf 6) # cyan
+fgWhite=$(tput setaf 7) # white
+
+# text editing options
+
+txBold=$(tput bold)   # bold
+txHalf=$(tput dim)    # half-bright
+txUnderline=$(tput smul)   # underline
+txEndUnder=$(tput rmul)   # exit underline
+txReverse=$(tput rev)    # reverse
+txStandout=$(tput smso)   # standout
+txEndStand=$(tput rmso)   # exit standout
+txReset=$(tput sgr0)   # reset attributes
