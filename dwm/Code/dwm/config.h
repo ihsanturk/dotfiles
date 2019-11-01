4328ae6a7fa3f10999e96960b3ab5e57e314a720
ihsan <aliihsanturk@gmail.com>
2019-11-01 19:11:24 +0300

    Edit: font is monospace instead of Inconsolata

--- dwm/Code/dwm/config.h
+++ dwm/Code/dwm/config.h
@@ -11,7 +11,7 @@ static const unsigned int snap      = 16; /* snap pixel */
 static const int showbar            = 0;  /* 0 means no bar */
 static const int topbar             = 1;  /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=16" };
-static const char dmenufont[]       = "Inconsolata:size=17";
+static const char dmenufont[]       = "monospace:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
