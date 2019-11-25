2753320749406baefaf35925315f8168c6454632
ihsan <aliihsanturk@gmail.com>
2019-11-25 15:39:27 +0300

    [dwm]: 0 gaps, show bar

--- dwm/Code/dwm/config.h
+++ dwm/Code/dwm/config.h
@@ -7,9 +7,9 @@
 
 /* appearance */
 static const unsigned int borderpx = 2;  /* border pixel of windows */
-static const unsigned int gappx    = 50; /* was 35 *//* gaps between windows */
+static const unsigned int gappx    = 0; /* was 35 *//* gaps between windows */
 static const unsigned int snap     = 16; /* snap pixel */
-static const int showbar           = 0;  /* 0 means no bar */
+static const int showbar           = 1;  /* 0 means no bar */
 static const int topbar            = 1;  /* 0 means bottom bar */
 static const char *fonts[]         = { "monospace:size=16" };
 static const char dmenufont[]      = "monospace:size=16";
