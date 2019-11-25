9151641d22a7b9b27c0e1029f77f84f99fd13e80
ihsan <aliihsanturk@gmail.com>
2019-11-25 17:50:21 +0000

    [alias]: pacman aliases

--- Optional/packages.sh
+++ Optional/packages.sh
@@ -2,44 +2,51 @@
 
 . ./optional.conf 
 
-DISTRO=$(lsb_release -i | xargs echo | sed 's/Distributor ID: //g')
+##=== Distro Detection ===##
 
-if [ "$DISTRO" = "Ubuntu" ]; then
-  packageManager="apt install";
-elif [ "$DISTRO" = "Arch" ]; then
-  packageManager="pacman -S";
-elif [ "$DISTRO" = "Void" ]; then
-  packageManager="xbps-install -S";
-else
-  echo "OS not recognized!";
-  exit 2;
-fi
+#DISTRO=$(lsb_release -i | xargs echo | sed 's/Distributor ID: //g')
+#if [ "$DISTRO" = "Ubuntu" ]; then
+#  packageManager="apt install";
+#elif [ "$DISTRO" = "Arch" ]; then
+#  packageManager="pacman -S";
+#elif [ "$DISTRO" = "Void" ]; then
+#  packageManager="xbps-install -S";
+#else
+#  echo "OS not recognized!";
+#  exit 2;
+#fi
+
+##=== / Distro Detection / ===##
+
+packageManager="pacman -S"
+arguments="--noconfirm"
 
 pkgList="\
 	fzf\
-	git\
 	nnn\
-	curl\
+	zsh\
+	mosh\
 	stow\
-	dmenu\
-	sxhkd\
-	xclip\
-	hdparm\
 	neovim\
-	usbmuxd\
-	chromium\
-	libnotify\
-	xbindkeys\
-	alsa-utils\
 	base-devel\
-	libX11-devel\
-	libXft-devel\
-  wl-clipboard
-	xorg-minimal\
-  translate-shell
-	xf86-video-intel\
+  python-pip\
 "
+	#sxhkd\
+	#xclip\
+	#dmenu\
+	#hdparm\
+	#usbmuxd\
+	#chromium\
+	#libnotify\
+	#xbindkeys\
+	#alsa-utils\
+	#libX11-devel\
+	#libXft-devel\
+	#wl-clipboard\
+	#xorg-minimal\
+	#translate-shell\
+	#xf86-video-intel\
 
 echo "$pkgList" | tr ' ' '\n' | while read item; do
-   sudo $packageManager $item;
+   sudo $packageManager $arguments $item;
 done
