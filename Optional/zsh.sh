dcdb4cbfb2b166d256c6e3087338d1a332fabaee
ihsan <aliihsanturk@gmail.com>
2019-11-25 17:52:07 +0000

    Add: zsh.sh auto install ohmyzsh and some plugins

---
+++ Optional/zsh.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Set default shell zsh
+ sudo pacman -S zsh
+ sudo chsh -s $(which zsh) $USERNAME
+ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
