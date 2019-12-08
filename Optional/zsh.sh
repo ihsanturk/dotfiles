bcc5c157ddb5edfece498edc5299a07c979492b2
ihsan <ihsanl@protonmail.com>
2019-12-08 12:33:28 +0300

    [zsh.sh]: Fix: zsh custom path

--- Optional/zsh.sh
+++ Optional/zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Set default shell zsh
-sudo pacman -S zsh
+## Set default shell zsh
+# sudo pacman -S zsh
 sudo chsh -s $(which zsh) $USERNAME
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -9,6 +9,6 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 #Fzf-zsh
-git clone https://github.com/junegunn/fzf.git ${ZSH}/custom/plugins/fzf
-${ZSH}/custom/plugins/fzf/install --bin
-git clone https://github.com/Treri/fzf-zsh.git ${ZSH}/custom/plugins/fzf-zsh
+git clone https://github.com/junegunn/fzf.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/fzf
+${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/fzf/install --bin
+git clone https://github.com/Treri/fzf-zsh.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/fzf-zsh
