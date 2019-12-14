7814d9cc683109652cbebb728d93219057516ac4
ihsan <ihsanl@protonmail.com>
2019-12-14 13:15:58 +0300

    [brew]: Add new packages

--- Optional/BrewPackages.sh
+++ Optional/BrewPackages.sh
@@ -1,11 +1,13 @@
 brew install z
-brew install zsh
 brew install fzf
+brew install zsh
+brew install mosh
 brew install stow
 brew install tmux
 brew install neovim
-brew install python3
 brew install cointop
+brew install python3
 brew install qrencode
 brew install mitmproxy
+brew install youtube-dl
 brew cask install kitty
