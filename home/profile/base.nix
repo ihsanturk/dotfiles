46e73c4c8138c8062b7d421b49d3cf4d50d9dced
ihsan <ihsanl@pm.me>
2020-11-15 21:37:47 +0300

    update packages

--- home/profile/base.nix
+++ home/profile/base.nix
@@ -20,22 +20,24 @@
 		tldr
 		gnupg
 		groff
+		ranger
 		gnumake
-		nix-doc
 		ripgrep
 		tmpmail
 		# cachix
+		gnuchess
 		recutils
 		# docopts
 		moreutils
 		syncthing
 		trash-cli
-		neuron-notes
 		neovim-remote
+		# neuron-notes
 		# bitwarden-cli
 		coreutils # tac
 		translate-shell
-		# asciidoctor # ruby is cannot be compiled
+		# nix-doc # NOTE: cannot be compiled
+		# asciidoctor # NOTE: ruby cannot be compiled
 		gitAndTools.delta # for: git diff # long compile time
 		# slack # using: https://slack.com/intl/en-gb/downloads/instructions/mac
 
@@ -49,14 +51,14 @@
 	programs.git.enable = true;
 	programs.vim.enable = true;
 	programs.zsh.enable = true;
-	# programs.lf.enable = true;
 	programs.tmux.enable = true;
-	# programs.fish.enable = true;
 	programs.neovim.enable = true;
 	programs.starship.enable = true;
-	# services.gpg-agent.enable = true;
 
 	# # rare
+	# programs.lf.enable = true;
+	# programs.fish.enable = true;
+	# services.gpg-agent.enable = true;
 	# home.packages = with pkgs; [
 	# 	niv
 	# 	w3m
