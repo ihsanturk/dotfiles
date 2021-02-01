af1a7b4e8b4cf048200f3a69cb69b789c4d40a1f
ihsan <ihsanl@pm.me>
2021-02-01 17:00:52 +0300

    put back the emacs config, auto install packer

---
+++ config/neovim/plugins.lua
@@ -0,0 +1,17 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer in your `opt` pack
+vim.cmd [[packadd packer.nvim]]
+
+-- Only if your version of Neovim doesn't have https://github.com/neovim/neovim/pull/12632 merged
+vim._update_package_paths()
+
+return require('packer').startup(function()
+
+	use {'wbthomason/packer.nvim', opt = true}
+	use 'lifepillar/vim-gruvbox8'
+	use '9mm/vim-closer'
+	use 'LnL7/vim-nix'
+
+end)
+
