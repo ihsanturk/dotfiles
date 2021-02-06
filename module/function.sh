140c29fb8e255217a77fd3ebfc706de312838532
ihsan <ihsanl@pm.me>
2021-02-06 21:15:31 +0300

    add code() func for shell, search immediately zk

--- module/function.sh
+++ module/function.sh
@@ -1,8 +1,3 @@
-learn() {
-	learndir=~/code/github.com/ihsanturk/learn
-	cd $learndir/$1
-}
-
-zk() {
-	cd ~/zk && nvim
-}
+code() { cd $DIR_CODE/$1; }
+learn() { cd $DIR_LEARN/$1; }
+zk() { cd ~/zk && nvim +norm\ gzz; }
