e734f311addcf694a7b74b9e1267f864a6089be1
ihsan <ihsanl@pm.me>
2021-02-06 23:42:44 +0300

    add: subcommands for zk() function

--- module/function.sh
+++ module/function.sh
@@ -1,3 +1,8 @@
 code() { cd $DIR_CODE/$1; }
 learn() { cd $DIR_LEARN/$1; }
-zk() { cd ~/zk && nvim +norm\ gzz; }
+zk() {
+	cd ~/zk && case $1 in
+		(n|ne|new) nvim +norm\ gzn;;
+		(s|se|sea|sear|search) nvim +norm\ gzz;;
+	esac
+}
