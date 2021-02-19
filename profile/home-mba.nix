daa3fd59edee384575542f181954713ddbf0a220
ihsan <ihsanl@pm.me>
2021-02-20 02:02:31 +0300

    add, annotaions for packages, tidy up

--- profile/home-mba.nix
+++ profile/home-mba.nix
@@ -17,29 +17,29 @@
 
 	home.packages = with pkgs; [
 
-		mpv
-		groff
-		m-cli
-		ffmpeg
-		ledger
-		pandoc
-		svgbob
-		awscli2
-		graphviz
-		qrencode
-		coreutils
-		youtube-dl
-		imagemagick
-		neuron-notes
-		unixtools.watch
-		ncdu # clear out your storage devices
-		poppler_utils # pdftotext # for ripgrep-all
+		awscli2         # manage aws
+		coreutils       # extra coreutils
+		ffmpeg          # manipulate/edit video or image
+		graphviz        # visualize graphs
+		groff           # process text and rendering
+		imagemagick     # manipulate/convert images
+		ledger          # manage money and budgets
+		m-cli           # swiss army knife for macos
+		mpv             # play video, image or audio
+		ncdu            # clear out your storage devices
+		neuron-notes    # manage zettelkasten notes
+		pandoc          # convert across text formats
+		poppler_utils   # pdftotext # for ripgrep-all
+		qrencode        # encode data to qr codes
+		svgbob          # convert ascii to closest svg
+		unixtools.watch # better `watch`
+		youtube-dl      # download video from web
 
 		# # problematic
-		# img2txt # (personal) requires docopts 
-		# tmpmail # FIXME: make this flake compatible
-		# swiftbar  # NOTE: uninstall manually: "/Applications/SwiftBar.app"
-		# grap # https://www.lunabase.org/~faber/Vault/software/grap/
+		# img2txt       # (personal) requires docopts 
+		# tmpmail       # FIXME: make this flake compatible
+		# swiftbar      # NOTE: uninstall manually: "/Applications/SwiftBar.app"
+		# grap          # https://www.lunabase.org/~faber/Vault/software/grap/
 		# (let trends = builtins.fetchTarball "https://github.com/ihsanturk/trends/archive/master.tar.gz"; in import trends {})
 
 	];
