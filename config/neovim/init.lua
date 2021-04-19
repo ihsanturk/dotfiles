0edefee089cf05707995cda51197c52af8dd1029
ihsan <ihsanl@pm.me>
2021-04-19 11:18:35 +0300

    disable nvim netrw banner

--- config/neovim/init.lua
+++ config/neovim/init.lua
@@ -26,6 +26,7 @@ vim.g.loaded_node_provider   = 0
 vim.g.loaded_perl_provider   = 0
 vim.g.loaded_python_provider = 0
 vim.g.loaded_ruby_provider   = 0
+vim.g.netrw_banner = 0
 
 vim.bo.textwidth    = 80
 vim.o.autoindent    = true
