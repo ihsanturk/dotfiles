c0c321cb2ee79b3f25e285aa14c1f3dc12963284
ihsan <ihsanl@pm.me>
2021-05-11 16:46:56 +0300

    remove annotations that brokes syntax

--- profile/mba.nix
+++ profile/mba.nix
@@ -75,9 +75,9 @@ in {
 		text = pkgs.lib.mkDefault(pkgs.lib.mkAfter ''
 			set completion-ignore-case on
 			set completion-prefix-display-length 2
-			set show-all-if-ambiguous on         # completion: show on first tab
-			set show-all-if-unmodified on        # completion: show on first tab
-			set completion-map-case on           # completion: hypens = underscores
+			set show-all-if-ambiguous on
+			set show-all-if-unmodified on
+			set completion-map-case on
 		'');
 	};
 
