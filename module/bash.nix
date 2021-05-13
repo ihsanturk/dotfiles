7706f6f70d018e373a4fef9dc584c0d052be72d7
ihsan <ihsanl@pm.me>
2021-05-13 19:48:56 +0300

    immpediately append bash commands to history

--- module/bash.nix
+++ module/bash.nix
@@ -4,6 +4,7 @@
 	programs.bash = {
 		enable = true;
 		initExtra = builtins.readFile ./function.sh
+		          + builtins.readFile ./bash-init.sh
 		          + builtins.readFile ./sh-init.sh;
 	};
 
