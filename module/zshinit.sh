fd57f1b965c0a24fca95d88a2daa222d35de34e9
ihsan <ihsanl@pm.me>
2021-05-13 19:51:54 +0300

    bash compatible zsh history

--- module/zshinit.sh
+++ module/zshinit.sh
@@ -15,6 +15,7 @@ bindkey '^W' x-bash-backward-kill-word
 zstyle ':completion:*' completer _complete
 zstyle ':completion:*' matcher-list '' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+l:|=* r:|=*'
 
+unsetopt EXTENDED_HISTORY
 HISTFILE=~/.bash_history
 autoload -Uz compinit && compinit
 export WORDCHARS='*?_[]~=&;!#$%^(){}'
