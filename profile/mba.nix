818479dd39f607e5555a84eba7e4eebd351b1e1a
ihsan <ihsanl@pm.me>
2021-05-21 15:52:59 +0300

    increase key repeat

--- profile/mba.nix
+++ profile/mba.nix
@@ -141,8 +141,8 @@
 	};
 
 	# defaults
-	system.defaults.NSGlobalDomain.InitialKeyRepeat = 10;
-	system.defaults.NSGlobalDomain.KeyRepeat = 1;
+	system.defaults.NSGlobalDomain.InitialKeyRepeat = 15;
+	system.defaults.NSGlobalDomain.KeyRepeat = 2;
 	system.defaults.NSGlobalDomain._HIHideMenuBar = false;
 	system.defaults.dock.show-recents = false;
 	system.defaults.dock.tilesize = 33;
