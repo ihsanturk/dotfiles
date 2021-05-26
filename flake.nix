f24fa4ee8d757b4b1d2a70137159b68539cd5bbb
ihsan <ihsanl@pm.me>
2021-05-26 15:06:17 +0300

    major update

--- flake.nix
+++ flake.nix
@@ -1,122 +1,133 @@
 {
-	description = "ihsanturk's personal machine configurations.";
+  description = "ihsanturk's personal machine configurations.";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.darwin.url = "github:ihsanturk/nix-darwin";
+  inputs.darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  inputs.solar.url = "github:ihsanturk/solar";
+  inputs.vi.url = "github:ihsanturk/vi";
 
 
-	inputs.darwin.inputs.nixpkgs.follows = "nixpkgs";
-	inputs.darwin.url = "github:ihsanturk/nix-darwin";
-	inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
-	inputs.home-manager.url = "/Users/ihsan/code/github.com/nix-community/home-manager";
-	inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-	inputs.nixpkgs.url = "/Users/ihsan/code/github.com/nixos/nixpkgs";
-	inputs.solar.url = "github:ihsanturk/solar";
+  outputs =  { self,
+    darwin,
+    solar,  # TODO: make this to a custom nixpkgs repo like (ihsanpkgs)?
+    vi,
+    nixpkgs,
+    home-manager,
+    flake-utils,
+    neovim-nightly-overlay,
+    ...
+  }@inputs:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
 
+    overlays = [
+      inputs.neovim-nightly-overlay.overlay
+    ];
+    # pkgs = nixpkgs.legacyPackages.${system};
 
-	outputs =  { self,
-		darwin,
-		solar,  # TODO: make this to a custom nixpkgs repo like (ihsanpkgs)?
-		nixpkgs,
-		home-manager,
-		neovim-nightly-overlay,
-		...
-	}@inputs:
-	let
+    pkgs = import nixpkgs { inherit system; };
 
-		overlays = [ inputs.neovim-nightly-overlay.overlay ];
-		pkgs = nixpkgs.legacyPackages."x86_64-darwin"; # TODO: use flake-utils.system
-		impurePaths = [
+    impurePaths = [
 
-			"$HOME/.cargo/bin"
-			"$HOME/Library/Python/3.8/bin"
-			"$HOME/Sync/bin"
-			"/usr/local/mysql/bin"
+      "$HOME/.cargo/bin"
+      "$HOME/Library/Python/3.8/bin"
+      "$HOME/Sync/bin"
+      "/usr/local/mysql/bin"
 
-		];
-		completionIgnore = [
+    ];
+    completionIgnore = [
 
-			".egg-info"
-			".lock"
-			".o"
-			".pyc"
+      ".egg-info"
+      ".lock"
+      ".o"
+      ".pyc"
 
-		];
-	in {
+    ];
+  in {
 
-		homeConfigurations = {
+    nixpkgs.overlays = overlays;
 
-			ihsan = home-manager.lib.homeManagerConfiguration {
-				homeDirectory = /. + builtins.getEnv "HOME";
-				stateVersion = "21.05";
-				system = "x86_64-darwin";
-				username = "ihsan";
-				configuration = { pkgs, ... }: {
-					nixpkgs.overlays = overlays;
-					programs.home-manager.enable = true;
-					home.packages = import ./base.nix pkgs;
-					home.sessionVariables = rec {
-						DIR_CODE  = "$HOME/Sync/code";
-						DIR_LEARN = "$HOME/Sync/code/github.com/ihsanturk/learn";
-						EDITOR    = "nvim";
-						FIGNORE   = (builtins.concatStringsSep ":" completionIgnore);
-						GPG_TTY   = "$(tty)";
-						LANG      = "en_GB.UTF-8";
-						PATH      = (builtins.concatStringsSep ":" impurePaths) + ":$PATH";
-						VISUAL    = EDITOR;
-					};
-					imports = [
-						./module/autojump.nix
-						./module/bash.nix
-						./module/bat.nix
-						./module/fzf.nix
-						./module/git.nix
-						./module/h.nix
-						./module/ledger.nix
-						# ./module/neovim.nix
-						./module/tmux.nix
-						./module/zsh.nix
-						./module/neomutt.nix
-						./module/pass.nix
-						./dev-env/nix.nix
-					];
-				};
-			};
+    homeConfigurations = {
 
-		};
+      ihsan = home-manager.lib.homeManagerConfiguration {
+        homeDirectory = /. + builtins.getEnv "HOME";
+        inherit system;
+        stateVersion = "21.05";
+        username = "ihsan";
+        configuration = { pkgs, ... }: {
+          nixpkgs.overlays = overlays;
+          programs.home-manager.enable = true;
+          home.packages = import ./base.nix pkgs;
+          home.sessionVariables = rec {
+            DIR_CODE  = "$HOME/Sync/code";
+            DIR_LEARN = "$HOME/Sync/code/github.com/ihsanturk/learn";
+            EDITOR    = "nvim";
+            FIGNORE   = (builtins.concatStringsSep ":" completionIgnore);
+            GPG_TTY   = "$(tty)";
+            LANG      = "en_GB.UTF-8";
+            PATH      = (builtins.concatStringsSep ":" impurePaths) + ":$PATH";
+            VISUAL    = EDITOR;
+          };
+          imports = [
+            ./module/autojump.nix
+            ./module/bash.nix
+            ./module/bat.nix
+            ./module/fzf.nix
+            ./module/git.nix
+            ./module/h.nix
+            ./module/ledger.nix
+            ./module/neovim.nix
+            ./module/tmux.nix
+            ./module/zsh.nix
+            ./module/neomutt.nix
+            ./module/pass.nix
+            ./dev-env/nix.nix
+          ];
+        };
+      };
 
-		darwinConfigurations = {
+    };
 
-			MacBook-Air = darwin.lib.darwinSystem {
-				modules = [ (import ./profile/mba.nix) ];
-			};
+    darwinConfigurations = {
 
-		};
+      MacBook-Air = darwin.lib.darwinSystem {
+        modules = [ (import ./profile/mba.nix) ];
+      };
 
-		packages.x86_64-darwin = {
+    };
 
-			solar = inputs.solar.defaultPackage;
+    packages.x86_64-darwin = {
 
-			# source: https://github.com/LnL7/dotfiles/flake.nix
-			cpp = pkgs.callPackage
-				({ buildEnv, clang-unwrapped }:
-				buildEnv {
-					name = "${clang-unwrapped.name}-tools";
-					paths = [ ];
-					pathsToLink = [ "/bin" ];
-					postBuild = ''
-						mkdir -p $out/bin
-						ln -s ${clang-unwrapped}/bin/clangd $out/bin
-					'';
-				}) { inherit (pkgs.llvmPackages_9) clang-unwrapped; };
+      core = pkgs.callPackage
+        ({ buildEnv, bat, exa }:
+        buildEnv {
+          name = "ihsan-coreutils";
+          paths = [ bat exa ];
+          pathsToLink = [ "/bin" ];
+        }) { };
 
-		};
+      cat = pkgs.bat;
+      solar = inputs.solar.defaultPackage;
+      vi = inputs.vi.defaultPackage;
 
-		darwinPackages = self.darwinConfigurations."simple".pkgs;
-		defaultPackage.x86_64-darwin = self.darwinConfigurations.MacBook-Air.system;
+    };
 
-		apps.x86_64-darwin.home-manager = {
-			type = "app";
-			program = "${home-manager.defaultPackage.x86_64-darwin}/bin/home-manager";
-		};
+    darwinPackages = self.darwinConfigurations."simple".pkgs;
+    defaultPackage.x86_64-darwin = self.darwinConfigurations.MacBook-Air.system;
 
-	};
+    apps.${system}.home-manager = {
+      type = "app";
+      program = "${home-manager.defaultPackage.x86_64-darwin}/bin/home-manager";
+    };
+
+  });
 
 }
